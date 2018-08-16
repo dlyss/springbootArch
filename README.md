8de78137b8a5686ac3e9741ca01a7849ffcde0cf
dlyss <dongliangyuss@163.com>
2018-08-16 16:44:04 +0800

    完善内容

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 # ABOUT  
 springboot 骨架工程搭建，逐步从创建、配置mybatis、logback、多环境、多数据源、redis等，满足基本企业级开发即开即用的目的，降低工程搭建成本，提高效率。
 
+##注意事项  
+####tomcat部署   
+1、pom.xml  
+      
+      <packaging>war</packaging>    
+2、启动类继承SpringBootServletInitializer并重写configure方法  
+    
+    @SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }}  
+    
+####多环境部署  
+####多数据源  
+####redis相关   
+####logback   
+ 
--- src/main/java/com/baike/demo/SpringbootArchApplication.java
+++ src/main/java/com/baike/demo/SpringbootArchApplication.java
@@ -2,11 +2,17 @@ package com.baike.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.baike")
-public class SpringbootArchApplication {
+public class SpringbootArchApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootArchApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootArchApplication.class, args);
